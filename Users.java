5b7308a6b22fa6d94c1debf09c3481d2d04c4021
Ali Yılmaz <yilmazali170@gmail.com>
2023-10-08 18:51:42 +0300

    Initial commit

---
+++ Users.java
@@ -0,0 +1,55 @@
+package OthelloReversi;
+
+public class Users {
+    //kullanıcı bilgilerinin tutulduğu class
+
+    String ad;
+    String Soyad;
+    String kullaniciAdi;
+    String mail;
+    String sifre;
+
+
+
+    public String getKullaniciAdi() {
+        return kullaniciAdi;
+    }
+
+    public void setKullaniciAdi(String kullaniciAdi) {
+        this.kullaniciAdi = kullaniciAdi;
+    }
+
+    public String getAd() {
+        return ad;
+    }
+
+    public void setAd(String ad) {
+        this.ad = ad;
+    }
+
+    public String getSoyad() {
+        return Soyad;
+    }
+
+    public void setSoyad(String Soyad) {
+        this.Soyad = Soyad;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getSifre() {
+        return sifre;
+    }
+
+    public void setSifre(String sifre) {
+        this.sifre = sifre;
+    }
+
+
+}
